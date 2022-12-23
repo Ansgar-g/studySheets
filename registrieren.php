9bd01a70d972e8c3869e89f498a1f26824027162
Ansgar-g <102971959+Ansgar-g@users.noreply.github.com>
2022-12-23 12:08:10 +0100

    Create registrieren.php

---
+++ registrieren.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "";
+
+$user_username = $_POST['register_username'];
+$user_password = $_POST['register_password'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyUsers (username, pwd)
+VALUES ('$user_username', '$user_password')";
+
+$sql_sel = "SELECT id, firstname, lastname FROM MyUsers WHERE username='$user_username'";
+$result = $conn->query($sql_sel);
+
+if ($result->num_rows > 0) {
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}else{
+    echo "Benutzername bereits vergeben";
+}
+
+$conn->close();
+?> 
